62e5244292ff5b670650081981061c2bee6d2175
Joe Holdcroft <joe@message.co.uk>
2014-05-12 21:11:17 +0100

    Trying rpi-gpio package instead

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var gpio = require('pi-gpio');
+var gpio = require('rpi-gpio');
 
 // Open pin 7 (PIR) for input
-gpio.open(7, 'input', function(err) {
-	// Set timeout for every 500ms to check pin value
-	setTimeout(500, function() {
-		gpio.read(7, function(err, value) {
-			// Close pin 7 (PIR)
-			//gpio.close(7);
-			console.log(value);
-		});
-	});
+// gpio.open(7, 'input', function(err) {
+// 	// Set timeout for every 500ms to check pin value
+// 	setTimeout(500, function() {
+// 		gpio.read(7, function(err, value) {
+// 			// Close pin 7 (PIR)
+// 			//gpio.close(7);
+// 			console.log(value);
+// 		});
+// 	});
+// });
+
+gpio.on('change', function(channel, value) {
+	console.log('Channel ' + channel + ' value is now ' + value);
 });
+gpio.setup(7, gpio.DIR_IN);
