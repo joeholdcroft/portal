264f29166f89b6a82f56ae38b1ca44a203116d6c
Joe Holdcroft <joe@message.co.uk>
2014-05-13 23:44:11 +0100

    Test out audio

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-var gpio         = require('pi-gpio');
-var EventEmitter = require('events').EventEmitter;
-var events       = function() {};
-var movement     = false;
-
-util.inherits(events, EventEmitter);
+var audioPath = './audio/dojinsuite.mp3';
+var fs        = require('fs');
+var lame      = require('lame');
+var Speaker   = require('speaker');
+var gpio      = require('pi-gpio');
+var movement  = false;
 
 // Ensure pin 7 (PIR) is closed before trying to open it
 gpio.close(7);
+
+// Test start playing audio
+fs.createReadStream(audioPath)
+	.pipe(new lame.Decoder())
+	.on('format', function(format) {
+		this.pipe(new Speaker(format));
+	});
 
 // Open pin 7 (PIR) for input
 gpio.open(7, 'input', function(err) {
 	// Set interval for every 100ms to check pin value
 	setInterval(function() {
 		gpio.read(7, function(err, value) {
-			events.emit('movement', (1 == value));
-		}, 500);
+			newMovement = (1 == value);
+
+			if (newMovement !== movement) {
+				console.log(newMovement ? 'Movement detected' : 'Movement stopped');
+			}
+
+			movement = newMovement;
+		}, 100);
 	});
-});
-
-events.on('movement', function(val) {
-	movement = val;
-
-	console.log(movement ? 'Movement detected' : 'Movement stopped');
 });
