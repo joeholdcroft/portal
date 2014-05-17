095b282ef61f960010e4871c7917037f550f89de
Joe Holdcroft <joe@message.co.uk>
2014-05-17 18:25:58 +0100

    Crude trial for audio on movement

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
+// TODO: use events for movement toggle to tidy up code
+// TODO: loop the audio
+// TODO: fade audio in/out and mute when no movement instead of stopping
+
 var audioPath = './audio/dojinsuite.mp3';
 var fs        = require('fs');
 var lame      = require('lame');
 var Speaker   = require('speaker');
 var gpio      = require('pi-gpio');
 var movement  = false;
+var audioStream = null;
 
 // Ensure pin 7 (PIR) is closed before trying to open it
 gpio.close(7);
-
-// Test start playing audio
-fs.createReadStream(audioPath)
-	.pipe(new lame.Decoder())
-	.on('format', function(format) {
-		this.pipe(new Speaker(format));
-	});
 
 // Open pin 7 (PIR) for input
 gpio.open(7, 'input', function(err) {
@@ -24,6 +22,18 @@ gpio.open(7, 'input', function(err) {
 
 			if (newMovement !== movement) {
 				console.log(newMovement ? 'Movement detected' : 'Movement stopped');
+
+				if (newMovement && null === audioStream) {
+					audioStream = fs.createReadStream(audioPath)
+						.pipe(new lame.Decoder())
+						.on('format', function(format) {
+							this.pipe(new Speaker(format));
+						});
+				}
+				else if (!newMovement && null !== audioStream) {
+					audioStream.end();
+					audioStream = null;
+				}
 			}
 
 			movement = newMovement;
